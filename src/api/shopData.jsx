e6a7856b606725bb2e1871be46786aec3df21ca0
Setusudan <cryptiareta@gmail.com>
2022-06-22 22:19:20 +0200

    env maj

--- src/api/shopData.jsx
+++ src/api/shopData.jsx
@@ -1,7 +1,7 @@
 // Generate an api for all shop of paris
 
 export const getShopData = async () => {
-  const response = await fetch(`${process.env.REACT_APP_API_URL}/shop`);
+  const response = await fetch(`${process.env.VITE_APP_API_URL}/shop`);
   const data = await response.json();
   return data;
 };
