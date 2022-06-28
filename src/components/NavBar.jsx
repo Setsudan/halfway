191230114337d6b526bae9e5374c9882fcd2f21b
A_Rijenth <rijenth93100@laposte.net>
2022-06-28 17:33:18 +0200

    page1

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,61 @@
-const Nav = ({ type }) => {
+import { Link } from "react-router-dom";
+
+const ClientNav = () => {
+  return (
+    <div className="bar">
+      <Link to="">
+        <img src="https://www.svgrepo.com/show/9399/home.svg" alt="#" />
+      </Link>
+      <Link to="">
+        <img src="https://www.svgrepo.com/show/299682/qr-code.svg" alt="#" />
+      </Link>
+      <Link to="">
+        <img src="https://www.svgrepo.com/show/42768/cart.svg" alt="#" />
+      </Link>
+      <Link to="">
+        <img src="https://www.svgrepo.com/show/361411/account.svg" alt="#" />
+      </Link>
+    </div>
+  );
+};
+const DeliveryNav = () => {
+  return (
+    <div className="bar">
+      <Link to="">
+        <img src="#" alt="#" />
+      </Link>
+      <Link to="">
+        <img src="#" alt="#" />
+      </Link>
+      <Link to="">
+        <img src="#" alt="#" />
+      </Link>
+      <Link to="">
+        <img src="#" alt="#" />
+      </Link>
+    </div>
+  );
+};
+const BrandNav = () => {
+  return (
+    <div className="bar">
+      <Link to="">
+        <img src="#" alt="#" />
+      </Link>
+      <Link to="">
+        <img src="#" alt="#" />
+      </Link>
+      <Link to="">
+        <img src="#" alt="#" />
+      </Link>
+      <Link to="">
+        <img src="#" alt="#" />
+      </Link>
+    </div>
+  );
+};
+
+export const Nav = ({ type }) => {
   if (type == "brand") {
     return <BrandNav />;
   } else if (type == "delivery") {
@@ -6,59 +63,4 @@ const Nav = ({ type }) => {
   } else {
     return <ClientNav />;
   }
-};
-
-const ClientNav = () => {
-  return (
-    <>
-      <Link to="">
-        <img src="#" alt="#" />
-      </Link>
-      <Link to="">
-        <img src="#" alt="#" />
-      </Link>
-      <Link to="">
-        <img src="#" alt="#" />
-      </Link>
-      <Link to="">
-        <img src="#" alt="#" />
-      </Link>
-    </>
-  );
-};
-const DeliveryNav = () => {
-  return (
-    <>
-      <Link to="">
-        <img src="#" alt="#" />
-      </Link>
-      <Link to="">
-        <img src="#" alt="#" />
-      </Link>
-      <Link to="">
-        <img src="#" alt="#" />
-      </Link>
-      <Link to="">
-        <img src="#" alt="#" />
-      </Link>
-    </>
-  );
-};
-const BrandNav = () => {
-  return (
-    <>
-      <Link to="">
-        <img src="#" alt="#" />
-      </Link>
-      <Link to="">
-        <img src="#" alt="#" />
-      </Link>
-      <Link to="">
-        <img src="#" alt="#" />
-      </Link>
-      <Link to="">
-        <img src="#" alt="#" />
-      </Link>
-    </>
-  );
 };
