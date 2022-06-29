39d6f25322176badfb0e03cf8a867dd5075f4f88
Setusudan <cryptiareta@gmail.com>
2022-06-29 15:49:53 +0200

    last update from me

--- src/pages/Shop/QrCodeScanner.jsx
+++ src/pages/Shop/QrCodeScanner.jsx
@@ -1,11 +1,19 @@
 // This page will allow the shop to scan the client qr code to get the client's information
 
 export const QrCodeScanner = () => {
+  // ask the user for camera permission
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+  useEffect(() => {
+    (async () => {
+      const { status } = await Barttery.requestPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
   return (
     <>
       <h1>Scanner QR Code</h1>
-      {/* Faudra mettre le code pour scanner le QR code */
-}
+      {/* Faudra mettre le code pour scanner le QR code */}
     </>
   );
 };
